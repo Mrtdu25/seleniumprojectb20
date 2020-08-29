870bdd5380d8d3caa399bbf214c86c1d1f2fd120
Mrt1453701- <degirmenci_murat@yahoo.com>
2020-08-29 13:25:41 -0400

    AllClassNotes

---
+++ src/test/java/com/cybertek/tests/day3_/FacebookHREFVerification.java
@@ -0,0 +1,38 @@
+package com.cybertek.tests.day3_;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FacebookHREFVerification {
+    public static void main(String[] args) {
+        /*
+        TC #4: Facebook header verification
+        1.Open Chrome browser
+        2.Go to https://www.facebook.com
+        3.Verify “Create a page” link
+        href value contains text:
+        Expected: “registration_form”
+         */
+
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+
+        driver.get("https://www.facebook.com");
+
+        String expectedHREF="registration_form";
+       String actualHREF= driver.findElement(By.linkText("Create a Page")).getAttribute("href");
+
+       if (actualHREF.contains(expectedHREF)){
+           System.out.println("Facebook Header-href verification PASSED!");
+       }else {
+           System.out.println("Facebook Header-href Verification FAILED!");
+       }
+
+
+
+
+    }
+}
