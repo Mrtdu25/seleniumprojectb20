18150550834208dc76556c3e107c50e1e44756d4
Mrt1453701- <degirmenci_murat@yahoo.com>
2020-09-27 21:21:21 -0400

    AllClassNotes

--- src/test/java/com/cybertek/utilities/SmartBearUtilities.java
+++ src/test/java/com/cybertek/utilities/SmartBearUtilities.java
@@ -3,9 +3,12 @@ package com.cybertek.utilities;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
+import java.util.List;
+//iter for each loop & fori for loop
 public class SmartBearUtilities {
-
+    //1
     public static void loginToSmartBear(WebDriver driver){
         //3. Enter username: “Tester”
 
@@ -20,4 +23,37 @@ public class SmartBearUtilities {
         WebElement loginButton = driver.findElement(By.id("ctl00_MainContent_login_button"));
         loginButton.click();
     }
+
+    //2
+    public static void verifyOrder(WebDriver driver,String name){
+
+        List<WebElement> namesList = driver.findElements(By.xpath("//table[@id='ctl00_MainContent_orderGrid']//tr/td[2]"));
+
+        for (WebElement each : namesList) {
+            if (each.getText().equals(name)){
+                Assert.assertTrue(true);
+                return;
+            }
+        }
+        Assert.fail(name+" does not exist in the list Test FAILED!!");
     }
+
+    //3
+    public static void printNameAndCities(WebDriver driver){
+        List<WebElement> namesList = driver.findElements(By.xpath("//table[@id='ctl00_MainContent_orderGrid']//tr/td[2]"));
+        List<WebElement> citiesList = driver.findElements(By.xpath("//table[@id='ctl00_MainContent_orderGrid']//tr/td[7]"));
+
+        for (int i = 0; i <namesList.size() ; i++) {
+            System.out.println("Name"+ (i+1) +": " + namesList.get(i).getText() + ", City"+ (i+1) +": " + citiesList.get(i).getText());
+        }
+    }
+
+
+
+
+
+
+    }
+
+    //3
+
