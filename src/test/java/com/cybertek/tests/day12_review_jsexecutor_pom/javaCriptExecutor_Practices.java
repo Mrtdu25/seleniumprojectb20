c181e85b89c8099464351eefa255287a36b7b9c0
Duaa Degirmenci <mrtdua@Duaas-Air.fios-router.home>
2021-07-15 22:30:48 -0400

    js method added

--- src/test/java/com/cybertek/tests/day12_review_jsexecutor_pom/javaCriptExecutor_Practices.java
+++ src/test/java/com/cybertek/tests/day12_review_jsexecutor_pom/javaCriptExecutor_Practices.java
@@ -12,20 +12,16 @@ public class javaCriptExecutor_Practices {
     @Test
     public void scroll_using_jsexecutor_1(){
         //Get the page to scroll
-        Driver.getDriver().get("http://practice.cybertekschool.com/infinite_scroll");
+        Driver.getDriver().get("http://youtube.com");
 
         //Create instance of JSExecutor and cast our driver type to it.
         JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
 
         //use execute script method
-
-
-        for (int i = 0; i < 10 ; i++) {
+        for (int i = 0; i < 20 ; i++) {
             BrowserUtils.wait(1);
             js.executeScript("window.scrollBy(0, 500)");
-
         }
-
     }
 
     @Test
@@ -41,12 +37,29 @@ public class javaCriptExecutor_Practices {
         JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
 
         //use scrollIntoView function from JavaScript to scroll to a specific web element
-        BrowserUtils.wait(2);
+        BrowserUtils.wait(4);
         js.executeScript("arguments[0].scrollIntoView(true)", cybertekLink);
-        BrowserUtils.wait(2);
+        BrowserUtils.wait(4);
         js.executeScript("arguments[0].scrollIntoView(true)", homeLink);
 
 
+    }
+
+    @Test
+    public void scroll_using_jsexecutor_3(){
+        //Get the page to scroll
+        Driver.getDriver().get("http://youtube.com");
+
+        //Create instance of JSExecutor and cast our driver type to it.
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+
+        //use execute script metho
+            BrowserUtils.wait(1);
+            js.executeScript("window.scrollBy(0, 300000)");
+            BrowserUtils.wait(1);
+            js.executeScript("window.scrollBy(0, -300000)");
+            BrowserUtils.wait(3);
+            Driver.closeDriver();
     }
     @Test
     public void fill_form_using_javascript(){
@@ -57,15 +70,11 @@ public class javaCriptExecutor_Practices {
         WebElement signUpbutton = Driver.getDriver().findElement(By.name("wooden_spoon"));
 
         BrowserUtils.wait(1);
-
         JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
-
-        js.executeScript("arguments[0].setAttribute('value', 'Jane Doe')");
-
+        js.executeScript("arguments[0].setAttribute('value', 'Jane Doe')",usernameInput);
         BrowserUtils.wait(1);
         //sending keys to emailInput using JavaScript function .setAttribute
         js.executeScript("arguments[0].setAttribute('value', 'something@gmail.com')", emailInput);
-
         BrowserUtils.wait(1);
         //clicking to signUpbutton using JavaScript function
         js.executeScript("arguments[0].click()", signUpbutton);
